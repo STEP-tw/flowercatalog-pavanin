faac2857375ef3df45bbdf92df913cb46ebf075b
pavani <pavanikitty@gmail.com>
2018-01-09 18:23:32 +0530

    fixed displaying comments(before my guest is not showing previous comments)

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ let getContentType=function(url){
 
 let logRequest = (req,res)=>{
   let text = ['------------------------------',
+    `${timeStamp()}`,
     `${JSON.stringify(req.method,null,2)} ${JSON.stringify(req.url,null,2)}`,
     `HEADERS=> ${JSON.stringify(req.headers,null,2)}`,
     `COOKIES=> ${JSON.stringify(req.cookies,null,2)}`,
@@ -26,12 +27,6 @@ let logRequest = (req,res)=>{
   fs.appendFile('request.log',text,()=>{});
   console.log(`${req.method} ${req.url}`);
 };
-
-let resetCookies= (req,res)=>{
-  res.setHeader('Set-Cookie',`logInFailed=false,Expires=${new Date(1).toUTCString()}`);
-  res.setHeader('Set-Cookie',`sessionid=0,Expires=${new Date(1).toUTCString()}`);
-}
-
 
 let loadUser = (req,res)=>{
   let sessionid = req.cookies.sessionid;
@@ -54,10 +49,40 @@ let redirectToIndexPage=function(req,res){
   if(req.url=="/") res.redirect("/index.html");
 }
 
+
+let toHtml= function(comment){
+  return `<p>${comment.DATETIME} , ${comment.USERNAME} , ${comment.COMMENT}. </p>`;
+};
+
+let arrangeComments= function(comments){
+  return comments.map(toHtml).join("");
+};
+
+let getComments=()=>{
+  comments=fs.readFileSync("./dataBase/comments.json");
+  comments=JSON.parse(comments);
+  comments=arrangeComments(comments);
+  return comments;
+};
+
+let addComment=(comment)=>{
+  let commentsData=fs.readFileSync("./dataBase/comments.json");
+  commentsData=JSON.parse(commentsData);
+  commentsData.unshift(comment);
+  commentsData=JSON.stringify(commentsData);
+  fs.writeFileSync("./dataBase/comments.json",commentsData,"utf8");
+};
+
+let getCommentDetails= function(data){
+  let details={}
+  details=data;
+  details["DATETIME"]=timeStamp();
+  return details;
+}
+
 let app = WebApp.create();
 app.use(logRequest);
 app.use(loadUser);
-app.use(resetCookies);
 app.use(redirectToIndexPage);
 app.use(serveFileContent);
 app.post("/submit",(req,res)=>{
@@ -68,7 +93,10 @@ app.post("/submit",(req,res)=>{
     return;
   }
   let sessionid = new Date().getTime();
-  console.log(req.body);
+  let comment=getCommentDetails(req.body);
+  addComment(comment);
+  fs.writeFileSync("./public/comments.html",getComments(),"utf8")
+  res.setHeader('Set-Cookie',`logInFailed=false`);
   res.setHeader('Set-Cookie',`sessionid=${sessionid}`);
   user.sessionid = sessionid;
   res.redirect('./guestbook.html');
@@ -82,6 +110,7 @@ app.post("/login",(req,res)=>{
     return;
   }
   let sessionid = new Date().getTime();
+  res.setHeader('Set-Cookie',`logInFailed=false`);
   res.setHeader('Set-Cookie',`sessionid=${sessionid}`);
   user.sessionid = sessionid;
   res.redirect('./guestbook.html');
